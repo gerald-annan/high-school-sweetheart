62f5a455a1830c3fb71c3c3806c4cc840473d184
gerald-annan <geraldannan400@gmail.com>
2021-12-16 15:18:39 +0000

    Solved

--- lib/high_school_sweetheart.ex
+++ lib/high_school_sweetheart.ex
@@ -1,14 +1,19 @@
 defmodule HighSchoolSweetheart do
   def first_letter(name) do
     # Please implement the first_letter/1 function
+    String.trim(name)
+    |> String.at(0)
   end
 
   def initial(name) do
     # Please implement the initial/1 function
+    "#{first_letter(name) |> String.upcase()}."
   end
 
   def initials(full_name) do
     # Please implement the initials/1 function
+    Enum.map(String.split(full_name), &initial(&1))
+    |> Enum.join(" ")
   end
 
   def pair(full_name1, full_name2) do
@@ -28,5 +33,23 @@ defmodule HighSchoolSweetheart do
     #               *
 
     # Please implement the pair/2 function
+    love = "#{initials(full_name1)}  +  #{initials(full_name2)}"
+
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{love}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
   end
 end
